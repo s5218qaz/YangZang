88e1a76b4903030b4e8fd453b58474535340270d
pony <pony@pollex.com.tw>
2021-04-13 13:21:56 +0800

    首頁banner block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ProductSectionComponent } from './blocks/product-section/product-section.component';
+import { ProfileSectionComponent } from './blocks/profile-section/profile-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductSectionComponent,
+    ProfileSectionComponent,
   ],
   imports: [
     BrowserModule
